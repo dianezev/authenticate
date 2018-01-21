acc7bd52d90f603c1c8e80c99ba63e258a190fa5
Diane Zevenbergen <dianezev@comcast.net>
2018-01-20 20:02:46 -0800

    first commit

---
+++ routes/router.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+// GET route for reading data
+router.get('/', function (req, res, next) {
+  return res.sendFile(path.join(__dirname + '/templateLogReg/index.html'));
+});
+
+
+//POST route for updating data
+router.post('/', function (req, res, next) {
+//   confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords dont match");
+    return next(err);
+  }
+  
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf,
+    }
+    //use schema.create to insert data into the db
+    User.create(userData, function (err, user) {
+      if (err) {
+        return next(err)
+      } else {
+        return res.redirect('/profile');
+      }
+    });
+  }
+});
+
+module.exports = router;
